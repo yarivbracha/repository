90fe8896155431f2b7809bb08bb8e99e0edaff17
yariv10 <yariv_5@walla.com>
2016-07-01 12:27:57 +0300

    10

---
+++ Ex10.2_Yariv_Bracha/FileFinder/FileFinderHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+namespace FileFinder
+{
+    class FileFinderHelper
+    {
+        string[] files;
+        StreamReader reader;
+
+        public Dictionary<string, long> FindWordInFiles(string pathDirectory, string wordToFind)
+        {
+            string[] files = Directory.GetFiles(pathDirectory);
+            Dictionary<string, long> dictionaryFilesInfo = new Dictionary<string, long>();
+            for(int i = 0; i < files.Length; i++)
+            {
+                if (File.Exists(files[i]))
+                {
+                    try
+                    {
+                        FileStream fileStream = new FileStream(files[i], FileMode.Open, FileAccess.Read, FileShare.Read);
+                        reader = new StreamReader(fileStream);
+                        if (files[i].Contains(wordToFind))
+                        {
+                            FileInfo fileInfo = new FileInfo(files[i]);
+                            long fileLength = fileInfo.Length;
+                            dictionaryFilesInfo.Add(files[i], fileLength);
+                        }
+                    }
+                    finally
+                    {
+                        reader.Close();
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return dictionaryFilesInfo;
+        }
+    }
+}
---
+++ Ex10.2_Yariv_Bracha/FileFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileFinder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, long> filesInfoDictionary = new Dictionary<string, long>();
+            FileFinderHelper fileFinderHelper = new FileFinderHelper();
+            if (!(String.IsNullOrEmpty(args[0])) && (!String.IsNullOrEmpty(args[1])) || (args.Length != 2))
+            {
+                filesInfoDictionary = fileFinderHelper.FindWordInFiles(args[0], args[1]);
+                if (filesInfoDictionary.Count != 0)
+                {
+                    foreach (var info in filesInfoDictionary)
+                    {
+                        Console.WriteLine("The word [{0}] found in {1} and the file length is {2}", args[1], info.Key, info.Value);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There is no file names with the word {{0}}}", args[1]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is a null or empty parameter that passed from the command line");
+            }
+        }
+    }
+}
