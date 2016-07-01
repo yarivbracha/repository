2ac9243eb1e1b4bfde01651ad94e864d6de14d4a
yariv10 <yariv_5@walla.com>
2016-07-01 11:16:40 +0300

    Ex 10.1

---
+++ Ex10.1_Yariv_Bracha/Personnel/FileReaderHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Personnel
+{
+    class FileReaderHelper
+    {
+        StreamReader reader;
+
+        public FileReaderHelper(string path)
+        {
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            reader = new StreamReader(fileStream);
+        }
+
+        public void ReadFileLines()
+        {
+            string currentLine;
+            try
+            { 
+                List<string> fileLines = new List<string>();
+                while ((currentLine = reader.ReadLine()) != null)
+                {
+                    fileLines.Add(currentLine);
+                }
+                foreach (string line in fileLines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ Ex10.1_Yariv_Bracha/Personnel/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Personnel
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileReaderHelper helper = new FileReaderHelper(args[0]);
+            helper.ReadFileLines();
+        }
+            
+    }
+}
