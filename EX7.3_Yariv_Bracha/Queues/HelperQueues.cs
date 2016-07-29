96b3b3da7f9e40226ce0fd55e6f8ac9fdb502178
yariv10 <yariv_5@walla.com>
2016-07-29 17:13:10 +0300

    queue

---
+++ EX7.3_Yariv_Bracha/Queues/HelperQueues.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class HelperQueues
+    {
+        public void RunQueues()
+        {
+            LimitedQueue<int> queue = new LimitedQueue<int>(10);
+
+            for (int i = 0; i < 50; i++)
+            {
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    Random random = new Random();
+                    int newItem = random.Next(1, 50);
+                    if (newItem >= 25)
+                    {
+                        queue.Enque(newItem);
+                        Console.WriteLine("The operation is enque, queue: " + queue);
+                    }
+                    else
+                    {
+                        int dequeValue = queue.Deque();
+                        if (dequeValue != 0)
+                        {
+                            Console.WriteLine("The operation is deque, queue: " + queue);
+                        }
+                        else
+                        {
+                            Console.WriteLine("The deque rerjected!");
+                        }
+                    }
+                    Thread.Sleep(1000);
+                });
+            }
+            Console.Read();
+        }
+    }
+}
--- EX7.3_Yariv_Bracha/Queues/LimitedQueue.cs
+++ EX7.3_Yariv_Bracha/Queues/LimitedQueue.cs
@@ -1,33 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Queues
 {
     class LimitedQueue<T>
     {
         private Queue<T> queue;
-        private int capacity;
-        private SemaphoreSlim semaphore;
+        private Object lockObject;
+        private Semaphore semaphore;
 
         public LimitedQueue(int capacity)
         {
-            this.capacity = capacity;
-            semaphore = new SemaphoreSlim(capacity);
+            if (capacity > 0)
+            {
+                lockObject = new object();
+                semaphore = new Semaphore(capacity, capacity);
+                queue = new Queue<T>(capacity);
+            }
+            else
+            {
+                throw new Exception("Error! Invalid capacity queue!");
+            }
         }
 
         public void Enque(T item)
         {
+            semaphore.WaitOne();
             queue.Enqueue(item);
-            capacity++;
         }
 
-        public void Deque()
+        public T Deque()
         {
-             queue.Dequeue();
+            T item = default(T);
+            if (queue.Count > 0)
+            {
+                lock (lockObject)
+                {
+                    if (queue.Count > 0)
+                    {
+                        item = queue.Dequeue();
+                        semaphore.Release();
+                    }
+                }
+            }
+            return item;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return queue.Count;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            T[] tempArr = queue.ToArray();
+            if (queue.Count != 0)
+            {
+                sb.Append("[");
+                for(int i = 0; i < tempArr.Length; i++) 
+                {
+                    sb.Append(tempArr[i] + " ");
+                }
+                sb.Append("] ");
+                sb.AppendFormat("({0})", tempArr.Length);
+            }
+            else
+            {
+                sb.Append("No items in queue");
+            }
+            return sb.ToString();
         }
     }
 }
