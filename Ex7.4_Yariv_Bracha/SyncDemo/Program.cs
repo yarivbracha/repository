643408fd0f8ad03f3c0fb5d8cd32b60143f65cfe
yariv10 <yariv_5@walla.com>
2016-07-30 23:09:14 +0300

    y

--- Ex7.4_Yariv_Bracha/SyncDemo/Program.cs
+++ Ex7.4_Yariv_Bracha/SyncDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SyncDemo
@@ -10,7 +11,17 @@ namespace SyncDemo
     {
         static void Main(string[] args)
         {
+            SyncDemoHelper firstApp = new SyncDemoHelper();
+            SyncDemoHelper secondApp = new SyncDemoHelper();
+            //Task.Run(() => firstApp.RunSyncDemo());
+            //Task.Run(() => secondApp.RunSyncDemo());
+            Thread t1 = new Thread(() => firstApp.RunSyncDemo());
+            Thread t2 = new Thread(() => secondApp.RunSyncDemo());
+            t1.Start();
+            t2.Start();
+            Console.Read();
 
+            
         }
     }
 }
--- Ex7.4_Yariv_Bracha/SyncDemo/SyncDemoHelper.cs
+++ Ex7.4_Yariv_Bracha/SyncDemo/SyncDemoHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,6 +11,36 @@ namespace SyncDemo
 {
     class SyncDemoHelper
     {
-        Mutex mutex = new Mutex(false, "SyncFileMutex");
+       
+        public void RunSyncDemo()
+        {
+            Mutex mutex = new Mutex(false, "SyncFileMutex");
+            StreamWriter writer = null;
+            for (int i = 0; i < 10; i++)
+            {
+			    mutex.WaitOne();
+                try
+                {
+                    writer = new StreamWriter(@"c:\temp\data.txt", true);
+                    writer.WriteLine($"current Process: {Process.GetCurrentProcess().Id}");
+                    
+                }
+                catch (IOException)
+                {
+
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                    if (writer != null)
+                    {
+                        writer.Close();
+                    }
+                }
+            }
+            Console.WriteLine("Finish to write.");
+        }
     }
+
 }
+
