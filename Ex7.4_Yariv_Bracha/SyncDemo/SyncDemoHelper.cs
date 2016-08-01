e3ce2b5d4e3046f109bc60d5140613c7e006dd79
yariv10 <yariv_5@walla.com>
2016-08-01 09:43:09 +0300

    d

--- Ex7.4_Yariv_Bracha/SyncDemo/SyncDemoHelper.cs
+++ Ex7.4_Yariv_Bracha/SyncDemo/SyncDemoHelper.cs
@@ -16,7 +16,7 @@ namespace SyncDemo
         {
             Mutex mutex = new Mutex(false, "SyncFileMutex");
             StreamWriter writer = null;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10000; i++)
             {
 			    mutex.WaitOne();
                 try
