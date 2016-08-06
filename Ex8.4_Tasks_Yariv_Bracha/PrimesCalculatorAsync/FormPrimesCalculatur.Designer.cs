3b0b388bc9b7d4ba63366662e34fd56992d2ce73
yariv10 <yariv_5@walla.com>
2016-08-07 00:07:48 +0300

    hw

---
+++ Ex8.4_Tasks_Yariv_Bracha/PrimesCalculatorAsync/FormPrimesCalculatur.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PrimesCalculatorAsync
+{
+    partial class FormPrimesCalculatur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBoxFirstNumber = new System.Windows.Forms.TextBox();
+            this.textBoxSecondNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxOutputFile = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(12, 85);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(100, 33);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxFirstNumber
+            // 
+            this.textBoxFirstNumber.Location = new System.Drawing.Point(12, 30);
+            this.textBoxFirstNumber.Name = "textBoxFirstNumber";
+            this.textBoxFirstNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFirstNumber.TabIndex = 1;
+            // 
+            // textBoxSecondNumber
+            // 
+            this.textBoxSecondNumber.Location = new System.Drawing.Point(121, 30);
+            this.textBoxSecondNumber.Name = "textBoxSecondNumber";
+            this.textBoxSecondNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSecondNumber.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Second number:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(216, 107);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(35, 13);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "label3";
+            this.labelResult.Visible = false;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(12, 124);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(100, 33);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Result:";
+            // 
+            // textBoxOutputFile
+            // 
+            this.textBoxOutputFile.Location = new System.Drawing.Point(242, 30);
+            this.textBoxOutputFile.Name = "textBoxOutputFile";
+            this.textBoxOutputFile.Size = new System.Drawing.Size(220, 20);
+            this.textBoxOutputFile.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Output file:";
+            // 
+            // FormPrimesCalculatur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 177);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxOutputFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSecondNumber);
+            this.Controls.Add(this.textBoxFirstNumber);
+            this.Controls.Add(this.buttonCalculate);
+            this.Name = "FormPrimesCalculatur";
+            this.Text = "FormPrimesCalculatur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBoxFirstNumber;
+        private System.Windows.Forms.TextBox textBoxSecondNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxOutputFile;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
+
---
+++ Ex8.4_Tasks_Yariv_Bracha/PrimesCalculatorAsync/FormPrimesCalculatur.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimesCalculatorAsync
+{
+    public partial class FormPrimesCalculatur : Form
+    {
+        private int countPrimes;
+        private CancellationTokenSource cancellationToken;
+
+        public FormPrimesCalculatur()
+        {
+            InitializeComponent();
+            cancellationToken = new CancellationTokenSource();
+        }
+
+        private async void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = null;
+            object objToLock = new object();
+            string firstNumberString = textBoxFirstNumber.Text;
+            string secondNumberString = textBoxSecondNumber.Text;
+            string fileOutputName = textBoxOutputFile.Text;
+            if (String.IsNullOrEmpty(fileOutputName))
+            {
+                MessageBox.Show("Please enter output file name!");
+            }
+            else
+            {
+                bool isValidNumbers = IsValidNumbers(firstNumberString, secondNumberString);
+                if (!isValidNumbers)
+                {
+                    MessageBox.Show("Please enter valid numbers in the text boxes!");
+                }
+                else
+                {
+                    cancellationToken.Dispose();
+                    cancellationToken = new CancellationTokenSource();
+                    countPrimes = 0;
+                    labelResult.Visible = false;
+                    await Task.Run(() => CountPrimesAsync(firstNumberString, secondNumberString), cancellationToken.Token);
+                    try
+                    {
+                        writer = new StreamWriter(fileOutputName + ".txt", true);
+                    }
+                    catch(IOException)
+                    { }
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        labelResult.Text = countPrimes.ToString();
+                        writer.WriteLine($"Primes numbers count: {countPrimes.ToString()}");
+                        writer.Flush();
+                    }
+                    else
+                    {
+                        labelResult.Text = "Operation cancelled!!";
+                    }
+                    
+                    labelResult.Visible = true;
+                }
+                writer.Close();
+            }
+        }
+
+
+        public async void CountPrimesAsync(string firstNumberString, string secondNumberString)
+        {
+            int first = int.Parse(firstNumberString);
+            int second = int.Parse(secondNumberString);
+            List<int> listNumbers = new List<int>();
+            int max = Math.Max(first, second);
+            int min = Math.Min(first, second);
+            for (int i = min; i < max; i++)
+            {
+                if (i > 2)
+                {
+                    bool isPrime = true;
+                    double limit = Math.Sqrt(i);
+                    for (int j = 2; j <= limit; j++)
+                    {
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            MessageBox.Show("The operation cancelled!");
+                            return;
+                        }
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        countPrimes++;
+                    }
+                }
+            }
+        }
+
+        private bool IsValidNumbers(string firstNumber, string secondNumber)
+        {
+            bool isValid = true;
+            int first, second;
+            if (String.IsNullOrEmpty(firstNumber) || String.IsNullOrEmpty(secondNumber))
+            {
+                isValid = false;
+            }
+            else
+            {
+                if ((!int.TryParse(firstNumber, out first) || (!int.TryParse(secondNumber, out second))))
+                {
+                    isValid = false;
+                }
+            }
+            return isValid;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            cancellationToken.Cancel();
+        }
+    }
+}
