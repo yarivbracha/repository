c51098fe585c48d2506b4aaa0435d19fa07d21c9
yariv10 <yariv_5@walla.com>
2016-08-15 02:29:47 +0300

    Tasks5

---
+++ Ex8.5_Tasks_Yariv_Bracha/CustomAwaiter/AwaiterHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace CustomAwaiter
+{
+    class AwaiterHelper
+    {
+        public async Task RunByMillis(int millis)
+        {
+            if (millis > 0)
+            {
+                Console.WriteLine($"Await {millis} millisecondes - int ");
+                await millis;
+            }
+            else
+            {
+                Console.WriteLine("Illegal millisecondes value!");
+            }
+        }
+
+        public async Task RunBysecondes(int secondes)
+        {
+            if (secondes > 0)
+            {
+                Console.WriteLine($"Await {secondes} seconds - TimeSpan");
+                await TimeSpan.FromSeconds(secondes);
+            }
+            else
+            {
+                Console.WriteLine("Illegal secondes value!");
+            }
+        }
+
+        public async Task RunProcess(string processName)
+        {
+            try
+            {
+                Console.WriteLine($"Process {processName} - start...");
+                await Process.Start(processName);
+                Console.WriteLine($"Process {processName} exited.");
+            }
+            catch (Win32Exception)
+            {
+                Console.WriteLine($"The process name {processName} is not exist!!");
+            }
+        }
+    }
+}
---
+++ Ex8.5_Tasks_Yariv_Bracha/CustomAwaiter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace CustomAwaiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AwaiterHelper helper = new AwaiterHelper();
+            helper.RunByMillis(1000).Wait();
+            helper.RunBysecondes(2).Wait();
+            helper.RunProcess("notepad").Wait();
+        }
+    }
+
+    public static class AwaiterExtension
+    {
+       
+        public static TaskAwaiter GetAwaiter(this int millis)
+        {
+            return TimeSpan.FromMilliseconds(millis).GetAwaiter();
+        }
+
+        public static TaskAwaiter<int> GetAwaiter(this Process process)
+        {
+            var taskCompelitionSource = new TaskCompletionSource<int>();
+            process.EnableRaisingEvents = true;
+
+            process.Exited += (obj, eventArgs) => taskCompelitionSource.TrySetResult(process.ExitCode);
+            if (process.HasExited)
+            {
+                taskCompelitionSource.TrySetResult(process.ExitCode);
+            }
+
+            return taskCompelitionSource.Task.GetAwaiter();
+        }
+
+        public static TaskAwaiter GetAwaiter(this TimeSpan time)
+        {
+            return Task.Delay(time).GetAwaiter();
+        }
+
+    }
+}
